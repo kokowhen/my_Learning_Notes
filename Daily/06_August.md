6fa13bcb2384c976df95a75756bf13d7c579c7d5
Jeffery_glb <Jefferycool_glb@163.com>
2021-08-07 00:05:42 +0800

    806 home commit

---
+++ Daily/06_August.md
@@ -0,0 +1,16 @@
+# 今天要做的事情
+
+- **继续《爬虫认真学习系列》**
+
+- **认真学习正则表达式的知识，整理这个知识点的优质资料**
+
+# 实际完成情况
+
+- **在正则表达式这个知识点遇到很多问题，需要认真学习理解**
+
+- [**正则表达式菜鸟教程**](https://www.runoob.com/regexp/regexp-tutorial.html)
+
+- [**Python官方re模块文档**](https://docs.python.org/zh-cn/3/library/re.html)
+
+- 💛[**菜鸟学Python正则表达式**](https://mp.weixin.qq.com/s/pvG2VfksW1sPCCiEm8IBxA)
+
--- Python/Codes/Python爬虫认真学习系列/module_re.py
+++ Python/Codes/Python爬虫认真学习系列/module_re.py
@@ -2,27 +2,10 @@ import re
 import urllib.request
 
 
-#先理解re正则表达式的用法
+#先学习re正则表达式模块的基本用法
 
-contents = 'sd125845csefr81^&^*(^++_+)+_+0-9+%drg166d6sd511s615rs 	ldcsd22-——_'
-rerules = re.compile('\d')#封装规则，匹配数字字符
-rerules1 = re.compile('\D')#匹配非数字字符
-numbers = re.findall(rerules,contents)
-not_numbers = re.findall(rerules1,contents)
-print(numbers)
-print(not_numbers)
-
-#提取response里需要的信息
-
-#伪装浏览器对百度进行访问请求并返回响应
-
-url = 'https://www.baidu.com/'
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
-req = urllib.request.Request(url=url,headers=headers)
-response = urllib.request.urlopen(req).read().decode('utf-8')
-# print(response)
-
-#封装正则表达式的提取规则
-hot_news_name_rules = re.compile(r'<span class="title-content-title">(.*)</span>')
-hot_news_name = re.findall(hot_news_name_rules,response)
-print(hot_news_name)
+contents = 'dhsuue_237 jji _2rbhsjs'
+res = re.match('^dh.*\d+\s.*\s\D\d.*js$',contents)
+print(res)
+print(res.group())
+print(res.span())#re.match是用来判断字符串是否与正则表达式匹配的
