132ca3cfe0993737022d7d645c658ef6edd43b9b
Jeffery_company <Jeffery_company@163.com>
2021-08-06 17:04:28 +0800

    806 company commit

--- Python/Codes/Python爬虫认真学习系列/module_urllib.py
+++ Python/Codes/Python爬虫认真学习系列/module_urllib.py
@@ -4,14 +4,14 @@ import urllib.request
 
 url = 'https://www.baidu.com/'
 response = urllib.request.urlopen(url)
-print(response.read().decode('utf-8'))
+# print(response.read().decode('utf-8'))
 
 #伪装浏览器对百度进行访问请求并返回响应
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
 req = urllib.request.Request(url=url,headers=headers)
 response = urllib.request.urlopen(req)
-print(response.read().decode('utf-8'))
+# print(response.read().decode('utf-8'))
 
 #伪装浏览器对扇贝进行访问请求并返回响应
 
@@ -19,7 +19,7 @@ url1 = 'https://www.shanbay.com/'
 headers1 = {'user-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
 req1 = urllib.request.Request(url=url1,headers=headers1)
 response = urllib.request.urlopen(req1)
-print(response.read().decode('utf-8'))
+# print(response.read().decode('utf-8'))
 
 #伪装浏览器对晨星基金网进行访问请求并返回响应
 
